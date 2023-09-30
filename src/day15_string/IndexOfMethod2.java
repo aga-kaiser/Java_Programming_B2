349e785d4ef30ec02aa4a87f42faa1123e504594
aga-kaiser <agakaiser.pl@gmail.com>
2023-09-30 15:10:43 -0400

    more string methods

---
+++ src/day15_string/IndexOfMethod2.java
@@ -0,0 +1,33 @@
+package day15_string;
+/*
+    .charAt(index);
+    .indexOf("")
+    .indexOf('')
+    .indexOf('', startIndex)
+    .lastIndexOf("")
+    .lastIndexOf('')
+    .lastIndexOf('', startIndex)
+    .substring(startIndex);
+    .substring(startIndex, endIndex);
+
+ */
+public class IndexOfMethod2 {
+    public static void main(String[] args) {
+
+
+        String str = "definition";
+        //            0123456789
+        System.out.println("First i index: " + str.indexOf("i"));
+        System.out.println("First i index: " + str.indexOf('i')); // you can do it with character as well
+        System.out.println("Last i index" + str.lastIndexOf("i"));
+        System.out.println("Last i index" + str.lastIndexOf('i')); // you can do it with character as well
+
+
+
+        // how you can find the 2nd i
+        System.out.println("Second i index: " + str.indexOf('i', 4));
+        System.out.println("Second i index: " + str.indexOf('i', str.indexOf('i')+1));
+
+
+    }
+}
