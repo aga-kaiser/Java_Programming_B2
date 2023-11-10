588c767c92e4cb205e625a4c7a7a19b3e340d31e
aga-kaiser <agakaiser.pl@gmail.com>
2023-11-09 22:10:15 -0500

    constructors

---
+++ src/day32_custom_classes/TestChaining.java
@@ -0,0 +1,15 @@
+package day32_custom_classes;
+
+public class TestChaining {
+    public static void main(String[] args) {
+
+       new ChainExample();
+
+       System.out.println();
+       new ChainExample(5);
+
+        System.out.println();
+        new ChainExample("loopcamp");
+
+    }
+}
