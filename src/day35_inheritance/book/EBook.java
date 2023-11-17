e90c43318954e1e090b0df2c5259e9ce149563e6
aga-kaiser <agakaiser.pl@gmail.com>
2023-11-16 22:13:13 -0500

    encapsulation, inheritance intro

---
+++ src/day35_inheritance/book/EBook.java
@@ -0,0 +1,10 @@
+package day35_inheritance.book;
+
+public class EBook extends  Book {
+
+    int pages;
+
+    public void read(){
+        System.out.println("Reading a digital copy of " + title + " which has " + pages + " pages." );
+    }
+}
