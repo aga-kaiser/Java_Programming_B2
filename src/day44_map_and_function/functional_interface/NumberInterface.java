c456dd42ebfed811f0060081a19b43771ad5341c
aga-kaiser <agakaiser.pl@gmail.com>
2023-12-07 22:14:04 -0500

    map recap, enam, String vs StringBuildervs StringBuffer, Functional Interface Intro

---
+++ src/day44_map_and_function/functional_interface/NumberInterface.java
@@ -0,0 +1,8 @@
+package day44_map_and_function.functional_interface;
+//@FunctionalInterface
+public interface NumberInterface {
+
+    void apply(int n);
+    // "apply" or "test" are the common methods name used in functional interface
+
+}
